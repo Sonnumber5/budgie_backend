0d2fb32c446202c9f8ac5a9f0c13e2e854f39c20
Sonnumber5 <157999365+Sonnumber5@users.noreply.github.com>
2026-02-09 22:45:48 -0500

    defined auth types for login and registration

---
+++ src/types/index.ts
@@ -0,0 +1,38 @@
+import { Request } from "express";
+
+export interface AuthRequest extends Request {
+    user: {
+        userId: number;
+        email: string;
+        name: string;
+    };
+}
+
+export interface User {
+    id: number;
+    email: string;
+    password_hash: string;
+    name: string;
+    created_at: Date;
+    updated_at: Date;
+}
+
+export interface RegisterDTO {
+    email: string;
+    password: string;
+    name: string;
+}
+
+export interface loginDTO {
+    email: string;
+    password: string;
+}
+
+export interface AuthResponse {
+    token: string;
+    user: {
+        id: number;
+        email: string;
+        name: string;
+    }
+}
