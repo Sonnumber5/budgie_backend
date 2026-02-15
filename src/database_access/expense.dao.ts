65a2f6fc7b8ad07739c995ec0ea6786b32bc6aaa
Sonnumber5 <157999365+Sonnumber5@users.noreply.github.com>
2026-02-14 21:53:30 -0500

    added get expenses functionality

--- src/database_access/expense.dao.ts
+++ src/database_access/expense.dao.ts
@@ -7,4 +7,14 @@ export class ExpenseDAO{
         const result = await pool.query<Expense>(ExpenseQueries.CREATE_EXPENSE, [expenseDTO.userId, expenseDTO.categoryId, expenseDTO.vendor, expenseDTO.amount, expenseDTO.description, expenseDTO.expenseDate, expenseDTO.month]);
         return result.rows[0];
     }
+
+    async findExpensesByDate(userId: number, month: string): Promise<Expense[]>{
+        const result = await pool.query<Expense>(ExpenseQueries.FIND_EXPENSES_BY_DATE, [userId, month]);
+        return result.rows;
+    }
+
+    async findAllExpenses(userId: number): Promise<Expense[]>{
+        const result = await pool.query<Expense>(ExpenseQueries.FIND_ALL_EXPENSES, [userId]);
+        return result.rows;
+    }
 }
--- src/queries/expense.queries.ts
+++ src/queries/expense.queries.ts
@@ -1,4 +1,19 @@
 export const ExpenseQueries = {
-    CREATE_EXPENSE: `INSERT INTO expenses (user_id, category_id, vendor, amount, description, expense_date, month) VALUES ($1, $2, $3, $4, $5, $6, $7)
-                     RETURNING id, category_id, vendor, amount, description, expense_date`,
+    CREATE_EXPENSE: `INSERT INTO expenses (user_id, category_id, vendor, amount, description, expense_date, month) 
+                    VALUES ($1, $2, $3, $4, $5, $6, $7)
+                    RETURNING id, 
+                              category_id AS "categoryId", 
+                              vendor, 
+                              amount, 
+                              description, 
+                              expense_date AS "expenseDate", 
+                              created_at AS "createdAt", 
+                              updated_at AS "updatedAt"`,
+    FIND_EXPENSES_BY_DATE: `SELECT e.id, e.category_id AS "categoryId", e.vendor, e.amount, e.description, e.expense_date AS "expenseDate", e.created_at AS "createdAt", e.updated_at AS "updatedAt"
+                    FROM expenses e
+                    JOIN categories c ON c.id = e.category_id
+                    WHERE e.user_id = $1 AND e.month = $2`,
+    FIND_ALL_EXPENSES: `SELECT id, category_id AS "categoryId", vendor, amount, description, expense_date AS "expenseDate", created_at AS "createdAt", updated_at AS "updatedAt" 
+                        FROM expenses
+                        WHERE user_id = $1`
 }
